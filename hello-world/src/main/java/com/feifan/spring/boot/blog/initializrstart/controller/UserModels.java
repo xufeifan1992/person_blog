825bfc46c1c87734798aba934e8ce97e8dd6beb7
xufeifan <xufeifan1992@126.com>
2018-01-04 19:05:49 +0800

    初始化提交
    第一次提交

---
+++ hello-world/src/main/java/com/feifan/spring/boot/blog/initializrstart/controller/UserModels.java
@@ -0,0 +1,24 @@
+package com.feifan.spring.boot.blog.initializrstart.controller;
+
+import java.util.List;
+
+public class UserModels {
+	List<TestModel> users;
+
+	public List<TestModel> getUsers() {
+		return users;
+	}
+
+
+
+	public void setUsers(List<TestModel> users) {
+		this.users = users;
+	}
+
+
+
+	public UserModels() {
+		super();
+	}
+	
+}
